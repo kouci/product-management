127c7b4f6a2edf5e37c662cd0dc02493267bf2a8
kouci <mansourakociela@gmail.com>
2025-02-05 23:05:47 +0100

    feat(db): add V1__Add_date_creation_column_to_produit.sql migration file

---
+++ server/src/main/resources/db/migration/V1__Add_date_creation_column_to_produit.sql
@@ -0,0 +1,12 @@
+-- V2__Add_date_creation_column_to_produit.sql
+
+-- Ajouter la colonne date_creation à la table produit
+ALTER TABLE produit
+    ADD COLUMN date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+
+UPDATE produit
+SET date_creation = CURRENT_TIMESTAMP
+WHERE date_creation IS NULL;
+
+
